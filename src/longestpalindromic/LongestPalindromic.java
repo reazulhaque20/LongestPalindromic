c7eea21f7d3b29eab822289eba49d8023b53b0e6
Md. Reazul Haque <reazulhaque20@gmail.com>
2022-10-27 11:00:59 +0600

    145

--- src/longestpalindromic/LongestPalindromic.java
+++ src/longestpalindromic/LongestPalindromic.java
@@ -15,25 +15,13 @@ public class LongestPalindromic {
         if (N == 0) {
             return;
         }
-
-        // Position count
         N = 2 * N + 1;
-
-        // LPS Length Array
         int[] L = new int[N];
         L[0] = 0;
         L[1] = 1;
-
-        // centerPosition
         int C = 1;
-
-        // centerRightPosition
         int R = 2;
-
-        // currentRightPosition
         int i = 0;
-
-        // currentLeftPosition
         int iMirror;
         int expand = -1;
         int diff = -1;
@@ -41,57 +29,30 @@ public class LongestPalindromic {
         int maxLPSCenterPosition = 0;
         int start = -1;
         int end = -1;
-
-        // Uncomment it to print LPS Length array
-        // printf("%d %d ", L[0], L[1]);
         for (i = 2; i < N; i++) {
-            // Get currentLeftPosition iMirror
-            // for currentRightPosition i
             iMirror = 2 * C - i;
-
-            // Reset expand - means no
-            // expansion required
             expand = 0;
             diff = R - i;
-
-            // If currentRightPosition i is
-            // within centerRightPosition R
             if (diff >= 0) {
-
-                // Case 1
                 if (L[iMirror] < diff) {
                     L[i] = L[iMirror];
-                } // Case 2
+                }
                 else if (L[iMirror] == diff && R == N - 1) {
                     L[i] = L[iMirror];
-                } // Case 3
+                }
                 else if (L[iMirror] == diff && R < N - 1) {
                     L[i] = L[iMirror];
-
-                    // Expansion required
                     expand = 1;
-                } // Case 4
+                }
                 else if (L[iMirror] > diff) {
                     L[i] = diff;
-
-                    // Expansion required
                     expand = 1;
                 }
             } else {
                 L[i] = 0;
-
-                // Expansion required
                 expand = 1;
             }
-
             if (expand == 1) {
-
-                // Attempt to expand palindrome centered
-                // at currentRightPosition i. Here for odd
-                // positions, we compare characters and
-                // if match then increment LPS Length by ONE
-                // If even position, we just increment LPS
-                // by ONE without any character comparison
                 try {
                     while (((i + L[i]) < N
                             && (i - L[i]) > 0)
@@ -104,36 +65,18 @@ public class LongestPalindromic {
                     assert true;
                 }
             }
-
-            // Track maxLPSLength
             if (L[i] > maxLPSLength) {
                 maxLPSLength = L[i];
                 maxLPSCenterPosition = i;
             }
-
-            // If palindrome centered at
-            // currentRightPosition i expand
-            // beyond centerRightPosition R,
-            // adjust centerPosition C based
-            // on expanded palindrome.
             if (i + L[i] > R) {
                 C = i;
                 R = i + L[i];
             }
-
-            //Uncomment it to print LPS Length array
-            //System.out.print("%d ", L[i]);
         }
-
-        start = (maxLPSCenterPosition
-                - maxLPSLength) / 2;
+        start = (maxLPSCenterPosition - maxLPSLength) / 2;
         end = start + maxLPSLength - 1;
-
-        //System.out.print("start: %d end: %d\n",
-        //                  start, end);
-        System.out.print("LPS of string is "
-                + text + " : ");
-
+        System.out.print("LPS of string is " + text + " : ");
         for (i = start; i <= end; i++) {
             System.out.print(text.charAt(i));
         }
@@ -141,31 +84,22 @@ public class LongestPalindromic {
     }
 
     public static void main(String[] args) {
-        // TODO code application logic here
         String text1 = "babcbabcbaccba";
         findLongestPalindromicString(text1);
-
         String text2 = "abaaba";
         findLongestPalindromicString(text2);
-
         String text3 = "abababa";
         findLongestPalindromicString(text3);
-
         String text4 = "abcbabcbabcba";
         findLongestPalindromicString(text4);
-
         String text5 = "forgeeksskeegfor";
         findLongestPalindromicString(text5);
-
         String text6 = "caba";
         findLongestPalindromicString(text6);
-
         String text7 = "abacdfgdcaba";
         findLongestPalindromicString(text7);
-
         String text8 = "abacdfgdcabba";
         findLongestPalindromicString(text8);
-
         String text9 = "abacdedcaba";
         findLongestPalindromicString(text9);
     }
